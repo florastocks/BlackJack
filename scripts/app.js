72de5c05fa92eb79e6f44a4fecc55800ed03d9a0
florastocks <stocksflora@gmail.com>
2022-12-19 11:17:29 +0000

    scores being displayed - still displays dealer hidden card

--- scripts/app.js
+++ scripts/app.js
@@ -3,6 +3,8 @@ function init() {
   // ! Elements
   const hitBtn = document.querySelector('#hit')
   const stickBtn = document.querySelector('#stick')
+  const dScoreDisplay = document.querySelector('#dealer-score')
+  const pScoreDisplay = document.querySelector('#player-score')
 
   // ! Global Variables 
   let dealerScore = 0
@@ -33,8 +35,6 @@ function init() {
   function hit() {
     getRandomCard()
     dealPlayer()
-    
-    // console.log(playerScore)
   }
   hitBtn.addEventListener("click", hit);
 
@@ -42,7 +42,6 @@ function init() {
   function stick() {
     hitBtn.disabled = true;
     stickBtn.disabled = true;
-    //display player score
     console.log("player pressed stick")
     if (dealerScore < 17){
 
@@ -55,17 +54,16 @@ function init() {
   //! Player
   //* --------------------------------- need to fix the ace --------------------------------------------
 
-
   function dealPlayer(){
     getRandomCard()
     playerCards.push(selectedCard)
     playerScore += getScore(selectedCard)
-
-      if (playerScore > 21 && pAceCount > 0 ){
-        playerScore -= 10
-        pAceCount -= 1
-      }
-      console.log(playerScore)
+    pScoreDisplay.innerHTML = playerScore
+    if (playerScore > 21 && pAceCount > 0 ){
+      playerScore -= 10
+      pAceCount -= 1
+    }
+    console.log(playerScore)
     // add card images to html dynamically
     let cardImg = document.createElement("img")
     cardImg.src = `./images/${selectedCard}.png`
@@ -80,6 +78,7 @@ function init() {
       stickBtn.disabled = true;
       setTimeout(()=> alert('BUST! Dealer has won this time.'), 800)
     }
+  
   }
 
   function getScore(){
@@ -108,13 +107,14 @@ function init() {
     }
   }
 
-  // //! Dealer --------------------
+  // //! ------------------------- Dealer --------------------------
+
   function dealDealer(){
     getRandomCard()
     dealerCards.push(selectedCard)
     dealerScore += getDealerScore(selectedCard)
     unknown = dealerCards[0]
-
+    dScoreDisplay.innerHTML = dealerScore
     if (dealerScore > 21 && dAceCount > 0 ){
       dealerScore -= 10
       dAceCount -= 1
@@ -149,10 +149,7 @@ function init() {
 
 }
 init()
-
-  // if player score it 21  - remove buttons, and displayer winner. 
-  // if player score is > 21 && theres an A -> -10 from score. 
   // on page load  - click deal button to begin 
-  // need function for hit 
-  // function for stick 
-  // for loop - with i = 0; i <
+  // when player presses stick, remove the hidden card and show the real card 
+  // when dealer gets random card, if dealer cards length is 0, random card gets appended to the hidden card 
+  // need to show scores. 
